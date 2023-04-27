fb1994fcc7e258cb79b9051ea8e2f306ed81514f
s.kozel <kozel.sergey@gmail.com>
2023-04-27 14:30:37 +0200

    fix: playground

--- playground/App.tsx
+++ playground/App.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
-import {
-  Button,
-  StyleSheet,
-  Text,
-  TouchableWithoutFeedback,
-  View,
-} from 'react-native';
-import { Icon } from 'react-native-eva-icons';
+import { StatusBar } from 'expo-status-bar';
+import { Button, StyleSheet, Text, TouchableWithoutFeedback, View } from 'react-native';
 import GithubIcon from 'react-native-eva-icons/icons/Github';
+import { Icon } from 'react-native-eva-icons';
 
-export default (): React.ReactElement => {
+const IconColor = 'black';
 
+export default function App() {
   const zoomIconRef: React.RefObject<Icon> = React.useRef();
   const pulseIconRef: React.RefObject<Icon> = React.useRef();
   const shakeIconRef: React.RefObject<Icon> = React.useRef();
@@ -46,37 +42,37 @@ export default (): React.ReactElement => {
 
   return (
     <View style={styles.container}>
-
+      <StatusBar style='auto' />
       <View style={styles.showcaseContainer}>
         <Text>{'Default Icon\nIcons that are statically imported\nare not animateable'}</Text>
-        <GithubIcon {...styles.icon}/>
+        <GithubIcon {...styles.icon} fill={IconColor} />
       </View>
 
       <View style={styles.showcaseContainer}>
         <Text>{'Zoom Animation\nPress to animate. Second press to stop'}</Text>
         <TouchableWithoutFeedback onPress={onZoomIconPress}>
-          <Icon ref={zoomIconRef} width={64} height={64} name='maximize-outline' animation='zoom' />
+          <Icon ref={zoomIconRef} width={64} height={64} name='maximize-outline' animation='zoom' fill={IconColor} />
         </TouchableWithoutFeedback>
       </View>
 
       <View style={styles.showcaseContainer}>
         <Text>{'Pulse Animation\nPress to animate'}</Text>
         <TouchableWithoutFeedback onPress={onPulseIconPress}>
-          <Icon ref={pulseIconRef} width={64} height={64} name='activity' animation='pulse' />
+          <Icon ref={pulseIconRef} width={64} height={64} name='activity' animation='pulse' fill={IconColor} />
         </TouchableWithoutFeedback>
       </View>
 
       <View style={styles.showcaseContainer}>
         <Text>{'Shake Animation\nPress to animate'}</Text>
         <TouchableWithoutFeedback onPress={onShakeIconPress}>
-          <Icon ref={shakeIconRef} width={64} height={64} name='shake' animation='shake' />
+          <Icon ref={shakeIconRef} width={64} height={64} name='shake' animation='shake' fill={IconColor} />
         </TouchableWithoutFeedback>
       </View>
 
       <View style={styles.showcaseContainer}>
         <Text>{'Infinite Animation\nPress to start / end animation'}</Text>
         <TouchableWithoutFeedback onPress={onInfiniteAnimationIconPress}>
-          <Icon ref={infiniteAnimationIconRef} width={64} height={64} name='star' animationConfig={{ cycles: Infinity }} />
+          <Icon ref={infiniteAnimationIconRef} width={64} height={64} name='star' animationConfig={{ cycles: Infinity, useNativeDriver: true }} fill={IconColor} />
         </TouchableWithoutFeedback>
       </View>
 
@@ -87,7 +83,7 @@ export default (): React.ReactElement => {
 
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -102,7 +98,7 @@ const styles = StyleSheet.create({
     marginVertical: 8,
   },
   icon: {
-    // width: 64,
-    // height: 64,
+    width: 64,
+    height: 64,
   },
 });
--- playground/babel.config.js
+++ playground/babel.config.js
@@ -1,22 +1,6 @@
-const path = require('path');
-
-const moduleResolverConfig = {
-  root: path.resolve('./'),
-  alias: {
-    'react-native-eva-icons': path.resolve(__dirname, '../lib'),
-  },
-};
-
-module.exports = function (api) {
+module.exports = function(api) {
   api.cache(true);
-
-  const presets = [
-    'babel-preset-expo',
-  ];
-
-  const plugins = [
-    ['module-resolver', moduleResolverConfig],
-  ];
-
-  return { presets, plugins };
+  return {
+    presets: ['babel-preset-expo'],
+  };
 };
